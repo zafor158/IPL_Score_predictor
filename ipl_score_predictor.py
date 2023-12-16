f0b45a31a7c60884147202461fb9cbb91403434a
Md.Abu Zafor <98481506+zafor158@users.noreply.github.com>
2023-12-16 22:31:26 +0600

    Update ipl_score_predictor.py

--- ipl_score_predictor.py
+++ ipl_score_predictor.py
@@ -9,20 +9,11 @@ import streamlit as st
 #SET PAGE WIDE
 st.set_page_config(page_title='IPL_Score_Predictor',layout="centered")
 
-#Get the ML model
-filename ='IPL_Score_predictor/compressed_model.pkl.gz'
-#model = pickle.load(open(filename,'wb'))
-if os.path.exists(filename):
-    with gzip.open(filename, 'rb') as f:
-    model = pickle.load(f)
-else:
-    st.error(f"Error: File '{filename}' not found.")
+filename = 'IPL_Score_predictor/compressed_model.pkl.gz'
 
-try:
-    with gzip.open(filename, 'rb') as f:
-        loaded_model = pickle.load(f)
-except Exception as e:
-    st.error(f"Error loading the model: {e}")
+# Load the compressed pickle file
+with gzip.open(filename, 'rb') as f:
+    model = pickle.load(f)
 
 
 
