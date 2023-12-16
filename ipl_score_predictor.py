e49d62801df954ec56cbf5c58c5972567936ba7b
Md.Abu Zafor <98481506+zafor158@users.noreply.github.com>
2023-12-16 22:47:53 +0600

    Update ipl_score_predictor.py

--- ipl_score_predictor.py
+++ ipl_score_predictor.py
@@ -8,17 +8,13 @@ import streamlit as st
 
 #SET PAGE WIDE
 st.set_page_config(page_title='IPL_Score_Predictor',layout="centered")
-
 filename = 'compressed_model.pkl.gz'
 # Load the compressed pickle file
 with gzip.open(filename, 'rb') as f:
-    model = pickle.load(f)
+    labeled_model = pickle.load(f)
 #Title of the page with CSS
-
 st.markdown("<h1 style='text-align: center; color: white;'> IPL Score Prediction Using Machine Learning </h1>", unsafe_allow_html=True)
-
 #Add background image
-
 st.markdown(
          f"""
          <style>
@@ -61,7 +57,6 @@ elif batting_team == 'Royal Challengers Bangalore':
 elif batting_team == 'Sunrisers Hyderabad':
     prediction_array = prediction_array + [0,0,0,0,0,0,0,1]
 #SELECT BOWLING TEAM
-
 bowling_team = st.selectbox('Select the Bowling Team ',('Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab','Kolkata Knight Riders','Mumbai Indians','Rajasthan Royals','Royal Challengers Bangalore','Sunrisers Hyderabad'))
 if bowling_team==batting_team:
     st.error('Bowling and Batting teams should be different')
@@ -83,9 +78,7 @@ elif bowling_team == 'Royal Challengers Bangalore':
 elif bowling_team == 'Sunrisers Hyderabad':
     prediction_array = prediction_array + [0,0,0,0,0,0,0,1]
   
-
 col1, col2 = st.columns(2)
-
 #Enter the Current Ongoing Over
 with col1:
     overs = st.number_input('Enter the Current Over',min_value=5.1,max_value=19.5,value=5.1,step=0.1)
@@ -103,16 +96,13 @@ col3, col4 = st.columns(2)
 with col3:
 #Runs in last 5 over
     runs_in_prev_5 = st.number_input('Runs scored in the last 5 overs',min_value=0,max_value=runs,step=1,format='%i')
-
 with col4:
 #Wickets in last 5 over
     wickets_in_prev_5 = st.number_input('Wickets taken in the last 5 overs',min_value=0,max_value=wickets,step=1,format='%i')
-
 #Get all the data for predicting
-
 prediction_array = prediction_array + [runs, wickets, overs, runs_in_prev_5,wickets_in_prev_5]
 prediction_array = np.array([prediction_array])
-predict = model.predict(prediction_array)
+predict = labeled_model.predict(prediction_array)
 if st.button('Predict Score'):
     #Call the ML Model
     my_prediction = int(round(predict[0]))
