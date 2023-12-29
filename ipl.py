374229779ec6075f9d867696c46a66e86dca081f
Md.Abu Zafor <98481506+zafor158@users.noreply.github.com>
2023-12-29 12:20:13 +0600

    Add files via upload

---
+++ ipl.py
@@ -0,0 +1,193 @@
+# Importing Necessary Libraries
+import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
+#Importing dataset
+ipl_df = pd.read_csv('ipl_data.csv')
+print(f"Dataset successfully Imported of Shape : {ipl_df.shape}")
+
+irrelevant = ['mid', 'date', 'venue','batsman', 'bowler', 'striker', 'non-striker']
+#print(f'Before Removing Irrelevant Columns : {ipl_df.shape}')
+ipl_df = ipl_df.drop(irrelevant, axis=1) # Drop Irrelevant Columns
+#print(f'After Removing Irrelevant Columns : {ipl_df.shape}')
+# Define Consistent Teams
+const_teams = ['Kolkata Knight Riders', 'Chennai Super Kings', 'Rajasthan Royals',
+              'Mumbai Indians', 'Kings XI Punjab', 'Royal Challengers Bangalore',
+              'Delhi Daredevils', 'Sunrisers Hyderabad']
+
+ipl_df = ipl_df[(ipl_df['bat_team'].isin(const_teams)) & (ipl_df['bowl_team'].isin(const_teams))]
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+le = LabelEncoder()#This line creates an instance of the LabelEncoder class and assigns it to the variable le.
+#The LabelEncoder is used to encode categorical labels with numerical values.
+for col in ['bat_team', 'bowl_team']:
+  ipl_df[col] = le.fit_transform(ipl_df[col])
+
+from sklearn.compose import ColumnTransformer
+#ColumnTransformer is a useful tool for applying different transformers to different columns of a pandas DataFrame.
+columnTransformer = ColumnTransformer([('encoder',
+                                        OneHotEncoder(),  [0, 1])], remainder='passthrough')
+  
+ipl_df = np.array(columnTransformer.fit_transform(ipl_df))
+                                                                             
+cols = ['batting_team_Chennai Super Kings', 'batting_team_Delhi Daredevils', 'batting_team_Kings XI Punjab',
+              'batting_team_Kolkata Knight Riders', 'batting_team_Mumbai Indians', 'batting_team_Rajasthan Royals',
+              'batting_team_Royal Challengers Bangalore', 'batting_team_Sunrisers Hyderabad',
+              'bowling_team_Chennai Super Kings', 'bowling_team_Delhi Daredevils', 'bowling_team_Kings XI Punjab',
+              'bowling_team_Kolkata Knight Riders', 'bowling_team_Mumbai Indians', 'bowling_team_Rajasthan Royals',
+              'bowling_team_Royal Challengers Bangalore', 'bowling_team_Sunrisers Hyderabad', 'runs', 'wickets', 'overs',
+       'runs_last_5', 'wickets_last_5', 'total']
+df = pd.DataFrame(ipl_df, columns=cols)
+
+features = df.drop(['total'], axis=1)
+labels = df['total']
+features = df.drop(['total'], axis=1)
+labels = df['total']
+                                
+
+from sklearn.model_selection import train_test_split
+train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.20, shuffle=True)##
+
+
+from sklearn.ensemble import RandomForestRegressor
+forest = RandomForestRegressor()
+# Train Model
+forest.fit(train_features, train_labels)
+
+import pickle
+filename = "ml_model.pkl"
+pickle.dump(forest, open(filename, "wb"))
+
+
+#Streamlit App
+
+#import the libraries
+
+# %%writefile app.py
+import math
+import numpy as np
+import pickle
+import streamlit as st
+
+#SET PAGE WIDE
+st.set_page_config(page_title='IPL_Score_Predictor',layout="centered")
+
+#Get the ML model
+
+filename='ml_model.pkl'
+model = pickle.load(open(filename,'rb'))
+
+#Title of the page with CSS
+
+st.markdown("<h1 style='text-align: center; color: white;'> IPL Score Predictor Using machine Learning </h1>", unsafe_allow_html=True)
+
+#Add background image
+
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://images.app.goo.gl/r8nLcL7P42s4kqm46");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+#Add description
+
+with st.expander("Description"):
+    st.info("""A Simple ML Model to predict IPL Scores between teams in an ongoing match. To make sure the model results accurate score and some reliability the minimum no. of current overs considered is greater than 5 overs.
+
+ """)
+
+# SELECT THE BATTING TEAM
+
+
+batting_team= st.selectbox('Select the Batting Team ',('Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab','Kolkata Knight Riders','Mumbai Indians','Rajasthan Royals','Royal Challengers Bangalore','Sunrisers Hyderabad'))
+
+prediction_array = []
+  # Batting Team
+if batting_team == 'Chennai Super Kings':
+    prediction_array = prediction_array + [1,0,0,0,0,0,0,0]
+elif batting_team == 'Delhi Daredevils':
+    prediction_array = prediction_array + [0,1,0,0,0,0,0,0]
+elif batting_team == 'Kings XI Punjab':
+    prediction_array = prediction_array + [0,0,1,0,0,0,0,0]
+elif batting_team == 'Kolkata Knight Riders':
+    prediction_array = prediction_array + [0,0,0,1,0,0,0,0]
+elif batting_team == 'Mumbai Indians':
+    prediction_array = prediction_array + [0,0,0,0,1,0,0,0]
+elif batting_team == 'Rajasthan Royals':
+    prediction_array = prediction_array + [0,0,0,0,0,1,0,0]
+elif batting_team == 'Royal Challengers Bangalore':
+    prediction_array = prediction_array + [0,0,0,0,0,0,1,0]
+elif batting_team == 'Sunrisers Hyderabad':
+    prediction_array = prediction_array + [0,0,0,0,0,0,0,1]
+
+#SELECT BOWLING TEAM
+
+bowling_team = st.selectbox('Select the Bowling Team ',('Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab','Kolkata Knight Riders','Mumbai Indians','Rajasthan Royals','Royal Challengers Bangalore','Sunrisers Hyderabad'))
+if bowling_team==batting_team:
+    st.error('Bowling and Batting teams should be different')
+# Bowling Team
+if bowling_team == 'Chennai Super Kings':
+    prediction_array = prediction_array + [1,0,0,0,0,0,0,0]
+elif bowling_team == 'Delhi Daredevils':
+    prediction_array = prediction_array + [0,1,0,0,0,0,0,0]
+elif bowling_team == 'Kings XI Punjab':
+    prediction_array = prediction_array + [0,0,1,0,0,0,0,0]
+elif bowling_team == 'Kolkata Knight Riders':
+    prediction_array = prediction_array + [0,0,0,1,0,0,0,0]
+elif bowling_team == 'Mumbai Indians':
+    prediction_array = prediction_array + [0,0,0,0,1,0,0,0]
+elif bowling_team == 'Rajasthan Royals':
+    prediction_array = prediction_array + [0,0,0,0,0,1,0,0]
+elif bowling_team == 'Royal Challengers Bangalore':
+    prediction_array = prediction_array + [0,0,0,0,0,0,1,0]
+elif bowling_team == 'Sunrisers Hyderabad':
+    prediction_array = prediction_array + [0,0,0,0,0,0,0,1]
+
+
+col1, col2 = st.columns(2)
+
+#Enter the Current Ongoing Over
+with col1:
+    overs = st.number_input('Enter the Current Over',min_value=5.1,max_value=19.5,value=5.1,step=0.1)
+    if overs-math.floor(overs)>0.5:
+        st.error('Please enter valid over input as one over only contains 6 balls')
+with col2:
+#Enter Current Run
+    runs = st.number_input('Enter Current runs',min_value=0,max_value=354,step=1,format='%i')
+
+
+#Wickets Taken till now
+wickets =st.slider('Enter Wickets fallen till now',0,9)
+wickets=int(wickets)
+
+col3, col4 = st.columns(2)
+
+with col3:
+#Runs in last 5 over
+    runs_in_prev_5 = st.number_input('Runs scored in the last 5 overs',min_value=0,max_value=runs,step=1,format='%i')
+
+with col4:
+#Wickets in last 5 over
+    wickets_in_prev_5 = st.number_input('Wickets taken in the last 5 overs',min_value=0,max_value=wickets,step=1,format='%i')
+
+#Get all the data for predicting
+
+prediction_array = prediction_array + [runs, wickets, overs, runs_in_prev_5,wickets_in_prev_5]
+prediction_array = np.array([prediction_array])
+predict = model.predict(prediction_array)
+
+
+if st.button('Predict Score'):
+    #Call the ML Model
+    my_prediction = int(round(predict[0]))
+
+    #Display the predicted Score Range
+    x=f'PREDICTED MATCH SCORE : {my_prediction-5} to {my_prediction+5}'
+    st.success(x)
+
